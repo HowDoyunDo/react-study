4ef5cb47bf87a5bc49ae1d7c87fd65778e3da031
doyun <kjb1251@naver.com>
2021-02-21 19:28:29 +0900

    나머지 공부

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyPaser = require('body-parser')
+const { User } = require("./models/User")
+
+
+// application/x-www-form-urlencoded 해석
+app.use(bodyPaser.urlencoded({extended: true}));
+
+// application/json 해석
+app.use(bodyPaser.json());
+
 
 const mongoose = require('mongoose')
 
@@ -11,6 +21,26 @@ mongoose.connect('mongodb+srv://doyun:doyun@cluster0.uiudz.mongodb.net/test?retr
 }).then(() => console.log('Mongo DB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send(`Hello World! 하이`))
+app.get('/', (req, res) => res.send(`Hello World!.`))
+
+
+app.post('/register', (req, res) => {
+  // 회원 가입 시 필요한 정보들을 client에서 가져오면
+  // 그것들을 DB에 넣어준다.
+
+
+    const user = new User(req.body)
+
+    // user 모델에 req.body에서 파싱한 정보를 저장
+    user.save((err, doc) => {
+      if(err) return res.json({ success: false, err })
+      return res.status(200).json({
+        success: true
+      })
+    })
+
+  
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
